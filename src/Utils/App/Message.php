bfd853868a90dd2fe0a02c2c0138bee1890761fd
fatemehbarati <barati.it@tezlabs.com>
2017-09-13 21:51:45 +0430

    add message class to manage messages in other classes

---
+++ src/Utils/App/Message.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Barati
+ * Date: 9/12/2017
+ * Time: 05:13 PM
+ */
+
+namespace src\Utils\App;
+
+
+class Message
+{
+    const MANDATORY_SOURCE_DESTINATION = "Source and Destination are mandatory in a delivery note!";
+}
